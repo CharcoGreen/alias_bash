0559122f225b09fdf84b33a891fd31206cce62aa
CharcoGreen <dani.flaco0@gmail.com>
2018-05-31 14:00:17 +0200

    Update alias_user.sh

--- alias_user.sh
+++ alias_user.sh
@@ -5,6 +5,7 @@ alias upup='apt-get update && apt-get upgrade'
 # For user su
 # Alias for CentOS
 # Actualizar sistema
+alias onlyup='yum -y update'  
 alias upup='yum update && yum upgrade'
 # Restablecer red 
 alias restens33='ifdown ens33 && ifup ens33'
