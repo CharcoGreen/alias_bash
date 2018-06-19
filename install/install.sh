77d47036d20be321f75f60f430437b222dca64cf
root <root@localhost.localdomain>
2018-06-19 17:58:34 +0200

    modified:   install/install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 # AUTOR: Daniel Glez dani.flaco0@gmail.com
 # Descripción: Instalacion de software importante para entornos de comandos 
-# Habituales en mi entorno
+# Habituales en mi entorno CentOS
 
 # Usuario para docker
 UDocker='virtual'
 
 # Descarga y Actualizacion de paquetes
-yum update && yum upgrade -y 
+
+function update_upgrade(){
+
+	yum update && yum upgrade -y 
+
+}
 
 # Instalación de paquetes
 
@@ -19,7 +24,6 @@ function install_paq(){
 
 
 # Instalación de docker
-
 
 function remove_old_docker(){
 
@@ -34,6 +38,8 @@ function remove_old_docker(){
                   docker-engine-selinux \
                   docker-engine
 }
+
+# Instalación de docker
 
 function install_docker(){
 
@@ -52,6 +58,47 @@ function install_docker(){
 	usermod -aG docker $UDocker
 }
 
-# install_paq
-remove_old_docker
-install_docker
+# Instalar docker-machine
+
+function install_docker_machine(){
+
+	base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+  	curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  	sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+	
+	docker-machine version
+
+	# instalación de script complementarios 
+	
+	base=https://raw.githubusercontent.com/docker/machine/v0.14.0
+	for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
+	do
+  		wget "$base/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+	done
+}
+
+# Instalar docker compose
+
+function install_docker_compose(){
+
+	curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+	docker-compose --version
+}
+
+
+
+# init
+
+function init(){
+
+	update_upgrade
+	# install_paq
+	# remove_old_docker
+	# install_docker
+	# install_docker_machine
+	install_docker_compose
+}
+
+
+init
