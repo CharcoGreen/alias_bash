31ead3f8fc800a9af3cd9f4a5ed1d5f7c19f20d8
virtual <virtual@localhost.localdomain>
2018-06-19 12:03:45 +0200

    new file:   alias/alias_user.sh 	modified:   install/install.sh 	deleted:    alias_user.sh

---
+++ alias/alias_user.sh
@@ -0,0 +1,14 @@
+# Alias Debian
+
+# Upgrade system
+alias upup='apt-get update && apt-get upgrade'
+alias onlyup='apt-get update'
+
+# For user su
+# Alias for CentOS
+# Actualizar sistema
+alias onlyup='yum update'  
+alias upup='yum update && yum upgrade'
+# Restablecer red 
+alias restens33='ifdown ens33 && ifup ens33'
+#init
--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,57 @@
 #!/bin/bash
+# AUTOR: Daniel Glez dani.flaco0@gmail.com
+# Descripción: Instalacion de software importante para entornos de comandos 
+# Habituales en mi entorno
+
+# Usuario para docker
+UDocker='virtual'
+
+# Descarga y Actualizacion de paquetes
+yum update && yum upgrade -y 
+
+# Instalación de paquetes
+
+function install_paq(){
+
+	yum install -y git\
+		nano
+}
 
 
-# Declaracion de Alias
-alias	upup='yum update && yum upgrade -y'
-alias 	install='yum install -y'
+# Instalación de docker
 
 
-# Instalacion de software importante para entornos de comandos
+function remove_old_docker(){
 
-upup
-install git 
-install nano
+	yum remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+}
+
+function install_docker(){
+
+	# FINGERPRINT 060A 61C5 1B55 8A7F 742B 77AA C52F EB6B 621E 9F35"
+
+	yum install -y yum-utils \
+  	device-mapper-persistent-data \
+  	lvm2
+
+	yum-config-manager \
+	    	--add-repo \
+    		https://download.docker.com/linux/centos/docker-ce.repo
+	
+	yum install -y docker-ce
+	systemctl start docker
+	usermod -aG docker $UDocker
+}
+
+# install_paq
+remove_old_docker
+install_docker
