56fd05edaa11f92695bcd41412d1116211c65d19
CharcoGreen <dani.flaco0@gmail.com>
2018-09-12 11:05:27 +0200

    Update alias_user.sh

--- alias/alias_user.sh
+++ alias/alias_user.sh
@@ -12,3 +12,19 @@ alias upup='yum update && yum upgrade'
 # Restablecer red 
 alias restens33='ifdown ens33 && ifup ens33'
 #init
+
+
+# Docker Alias
+
+alias dps='docker ps'
+alias di='docker inspect '
+alias ds='docker service'
+alias dsvc='docker service  '
+alias dnet='docker network '
+alias dneti='docker network inspect '
+alias dnode='docker node '
+alias dvol='docker volume '
+alias dimages='docker images'
+alias dcont='docker ps -a'
+alias dsf='docker service ls --format '
+alias dexec='docker exec -ti '
